861448efa9f2a232bdf19948bcbd93ff12b21613
thisismynightmare <ericabross@gmail.com>
2019-04-13 15:36:05 -0500

    added featured work to homepage and created case study archive

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -21,9 +21,11 @@ get_header(); ?>
 		$link = get_field('site_link');
 		$image_1 = get_field('image_1');
 		$image_2 = get_field('image_2');
-		$image_3 = get_field('image_3'); ?>
+		$image_3 = get_field('image_3');
+		$size = "full"; 
+		?>
 
-	<article class="case-study">
+	<article class="case-study clearfix">
      	<aside class="case-study-sidebar">
          	<h2><?php the_title(); ?></h2>
          	<h4><?php echo $services; ?></h4>
@@ -31,31 +33,28 @@ get_header(); ?>
 
  			<?php the_content(); ?>
 
- 			<p><strong><a href="<?php echo $link; ?>">Visit Live Site &#8250</a></p>
+ 			<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site &#8250</a></p>
  		</aside>
 
 		<div class="case-study-images">
-			<?php if($image_1) { ?>
-				<img src="<?php echo $image_1; ?>" />
-			<?php } ?>
-
-			<?php if($image_2) { ?>
-				<img src="<?php echo $image_2; ?>" />
-			<?php } ?>
-
-			<?php if($image_3) { ?>
-				<img src="<?php echo $image_3; ?>" />
-			<?php } ?>
+			<?php if($image_1) {
+				echo wp_get_attachment_image($image_1, $size );
+			} ?>
+			<?php if($image_2) {
+				echo wp_get_attachment_image( $image_2, $size );
+			} ?>
+			<?php if($image_3) {
+				echo wp_get_attachment_image( $image_3, $size );
+			} ?>
 		</div>
-
-
-	<?php endwhile; // end of the loop. ?>
+	</article>
+		<?php endwhile; // end of the loop. ?>
 	</div><!-- .main-content -->
 
 </div><!-- #primary -->
 
 <nav id="navigation" class="container">
-	<div class="left"><span>BACK TO WORK</span></div>
+	<div class="left"><a href="<?php echo site_url('/case-studies/') ?>">&larr; <span>Back to Work</span></a></div>
 </nav>
 
 <?php get_footer(); ?>
